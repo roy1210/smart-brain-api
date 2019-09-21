9a355a8b62a77438f6ba58c425aecd2464701a2a
roy1210 <rswkure@gmail.com>
2019-09-21 11:49:26 +0800

    Added Clarifi api settings with .gitignore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+// SQL builder works with Node
 const knex = require('knex');
 
 const register = require('./controllers/register');
@@ -12,25 +13,36 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    // 127.0.0.1: means local host
+    host: '127.0.0.1',
+    user: '',
+    password: '',
+    database: 'smart-brain1'
   }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+  res.send(db.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
-})
+});
